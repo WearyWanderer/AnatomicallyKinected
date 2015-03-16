b571e0a72474c73d8e25a052a4ddd2895269ec43
Andrew Ayre <n3078472@live.tees.ac.uk>
2015-03-16 17:10:39 +0000

    Ready to go!

---
+++ Assets/Scripts/Gestures/GestureManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GestureManager : MonoBehaviour 
+{
+	public SkeletonWrapper skeletonWrapper;
+	public GameObject podium;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+
+
+	}
+
+	//If elbow in front of hips
+
+
+
+}
